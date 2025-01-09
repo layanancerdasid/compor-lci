a90e397920476bc9aa5bc8edb4fa332e12e5f3dd
nizaa07 <alfan9037@gmail.com>
2025-01-09 15:03:56 +0700

    add pages routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './home.jsx'
-import Navbar from './component/navbar.jsx'
-import Footer from './component/footer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./home.jsx";
+import Navbar from "./component/navbar.jsx";
+import Footer from "./component/footer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
